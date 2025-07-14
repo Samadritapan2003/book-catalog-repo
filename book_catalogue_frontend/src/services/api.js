35fdf6060cba41980388741652678251c46fe7a6
Samadritapan2003 <pansmadrita@gmail.com>
2025-07-15 02:44:35 +0530

    Fixed base URL issue for API

--- book_catalogue_frontend/src/services/api.js
+++ book_catalogue_frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`, // The environment variable + /api
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 export default API;
